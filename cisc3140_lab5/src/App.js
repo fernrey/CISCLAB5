764702c2df0cbbffc25bfd354ef2216a9679f65b
fernrey <Fernando39@live.com>
2022-05-12 15:11:50 -0400

    simple table

--- cisc3140_lab5/src/App.js
+++ cisc3140_lab5/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Address</th>
+            <th>Phone Number</th>
+            <th>Email </th>
+          </tr>
+        </thead>
+        <tbody>
+        <tr>
+          <td>Jenny Chan </td>
+          <td>3 waterfoot raod </td>
+          <td>blach balc </td>
+          <td> adsfasdfad </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
